295b83a7e118323f1add4219fbae8d53fd032515
Engkit Satia Riswara <engkitsatia.riswara@dana.id>
2019-01-30 15:06:02 +0700

    First init

---
+++ LatihanEngkit/Models/Engkit.swift
@@ -0,0 +1,9 @@
+//
+//  Engkit.swift
+//  LatihanEngkit
+//
+//  Created by Engkit Satia Riswara on 30/01/19.
+//  Copyright © 2019 Engkit Satia Riswara. All rights reserved.
+//
+
+import Foundation
